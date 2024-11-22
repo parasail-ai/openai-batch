0720c1884c7364c0e6d62fdd3ab52b701e661a35
Adam Lugowski <adam.lugowski@parasail.io>
2024-11-22 14:00:31 -0800

    Add wait()

---
+++ openai_batch/__init__.py
@@ -0,0 +1,5 @@
+from _utils import wait
+
+# OpenAI limits
+_FILE_MAX_REQUESTS = 50_000
+_FILE_MAX_BYTES = 100 * 1024 * 1024
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,3 +3,8 @@ import openai_batch
 
 def test_import():
     pass
+
+
+def test_batch_create_array():
+    prompts = ["Say Pong", "Hello"]
+    pass
