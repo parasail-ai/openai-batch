8ca7bb0edf6dc8cd9538937e89b6c8c7748f934c
Adam Lugowski <adam.lugowski@parasail.io>
2024-11-23 17:01:35 -0800

    Fix temp paths for Windows

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,4 +1,4 @@
-from tempfile import NamedTemporaryFile
+from tempfile import TemporaryDirectory
 from pathlib import Path
 
 import pytest
@@ -15,11 +15,11 @@ from openai_batch import example_prompts, create_input
     ids=["chat completion", "embedding"],
 )
 def test_example_prompts_script(args):
-    with NamedTemporaryFile() as prompt_file:
-        example_prompts.main([prompt_file.name] + args)
-        prompt_file.flush()
+    with TemporaryDirectory() as td:
+        prompts = Path(td) / "prompts.txt"
+        example_prompts.main([str(prompts)] + args)
 
-        assert 10 == len(Path(prompt_file.name).read_text().splitlines())
+        assert 10 == len(prompts.read_text().splitlines())
 
 
 @pytest.mark.parametrize(
@@ -31,16 +31,18 @@ def test_create_input_script(embedding):
     n = 10
     e = ["-e"] if embedding else []
 
-    with NamedTemporaryFile() as prompt_file, NamedTemporaryFile() as input_file:
+    with TemporaryDirectory() as td:
+        prompts = Path(td) / "prompts.txt"
+        input_file = Path(td) / "batch_input_file.txt"
+
         # create prompts
-        example_prompts.main([prompt_file.name, "-n", str(n)] + e)
-        prompt_file.flush()
+        example_prompts.main([str(prompts), "-n", str(n)] + e)
 
         # convert prompts to batch input file
-        create_input.main([prompt_file.name, input_file.name] + e)
+        create_input.main([str(prompts), str(input_file)] + e)
 
         # validate file
-        contents = Path(input_file.name).read_text()
+        contents = input_file.read_text()
         assert n == len(contents.splitlines())
 
         for line in contents.splitlines():
