66e1feda014439eccb669ca3e830415bd7ec10db
Adam Lugowski <adam.lugowski@parasail.io>
2024-12-04 17:28:44 -0800

    Add run script tests

--- openai_batch/run.py
+++ openai_batch/run.py
@@ -12,12 +12,13 @@ from . import wait
 from .providers import _add_provider_args, _get_provider
 
 
-def get_parser():
+def get_parser(argv):
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "input_file",
         type=str,
+        nargs="?" if "--resume" in argv else 1,
         help="Batch input file in .jsonl format. '-' will read from stdin.",
     )
 
@@ -51,9 +52,27 @@ def get_parser():
         action="store_true",
     )
 
+    parser.add_argument(
+        "--dry-run",
+        "-n",
+        help="Show what would happen but do not actually do it.",
+        default=False,
+        action="store_true",
+    )
+
     _add_provider_args(parser)
 
     return parser
+
+
+def count_lines(obj):
+    if isinstance(obj, bytes):
+        obj = obj.decode("utf-8")
+
+    if isinstance(obj, str):
+        return len(obj.splitlines())
+
+    return sum(1 for _ in obj)
 
 
 def write(client, file_id, fname, dash_stream):
@@ -67,17 +86,29 @@ def write(client, file_id, fname, dash_stream):
 
 
 def main(args=None):
-    args = get_parser().parse_args(args)
+    args = get_parser(args or sys.argv).parse_args(args)
     provider = _get_provider(args)
     client = OpenAI(base_url=provider.base_url, api_key=provider.api_key)
 
     batch_id = args.resume
 
     if not batch_id:
-        if args.input_file == "-":
+        if not args.input_file or not args.input_file[0]:
+            print("Please specify an input file, or '-' to read from stdin.")
+            return None
+
+        if args.input_file[0] == "-":
             input_file = sys.stdin.buffer.read()
         else:
-            input_file = open(args.input_file, "rb")
+            input_file = open(args.input_file[0], "rb")
+
+        if args.dry_run:
+            print(
+                f"Would start batch with {count_lines(input_file)} requests then " + "exit."
+                if args.create
+                else "wait for it to finish."
+            )
+            return None
 
         # Upload input file
         input_file = client.files.create(file=input_file, purpose="batch")
@@ -100,8 +131,12 @@ def main(args=None):
         print("This script will now wait for batch to finish, then it will download the output.")
         print(f"You may Ctrl+C and resume later with: --resume {batch_id}")
 
+    if args.dry_run:
+        print(f"Would wait until {batch_id} is complete.")
+        return batch_id
+
     # Wait for batch to complete
-    batch = wait(client, batch_id, callback=lambda b: print(f"Status of {b.id}: {b.status}"))
+    batch = wait(client, batch_id, callback=lambda b: print(f"Status of {batch_id}: {b.status}"))
 
     # Download output file
     if batch.output_file_id:
