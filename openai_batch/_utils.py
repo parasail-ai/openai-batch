c6aee26e0bf1b8bd66843a2d91aee3795fbc187f
Adam Lugowski <adam.lugowski@parasail.io>
2024-12-10 14:04:04 -0800

    Add multiple batch support to `wait()`

--- openai_batch/_utils.py
+++ openai_batch/_utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import time
-from typing import Any, Callable
+from typing import Any, Callable, Iterable, List
 
 import httpx
 import openai
@@ -22,32 +22,58 @@ FINISHED_STATES = ("failed", "completed", "expired", "cancelled")
 
 def wait(
     client: openai.Client,
-    batch_id: str,
+    batch_id: Iterable[str] | str,
     interval: float = 60,
     callback: Callable[[Batch], Any] = None,
+    finished_callback: Callable[[Batch], Any] = None,
     # Extras passed directly to the OpenAI client
     extra_headers: Headers | None = None,
     extra_query: Query | None = None,
     extra_body: Body | None = None,
     timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
-) -> Batch:
+) -> List[Batch] | Batch:
     """
-    Wait for batch to complete.
+    Wait for one or more batches to complete.
+
+    :param client: OpenAI API client.
+    :param batch_id: one or more batch IDs to wait on.
+    :param interval: How long to wait between each poll (in seconds).
+    :param callback: Called after each API retrieve.
+    :param finished_callback: Called once per finished batch.
+    :param extra_headers: Forwarded to OpenAI client.
+    :param extra_query: Forwarded to OpenAI client.
+    :param extra_body: Forwarded to OpenAI client.
+    :param timeout: Forwarded to OpenAI client.
+    :return: The batch object returned by the OpenAI client, or a list of these if ``batch_id`` is a list.
     """
 
-    while True:
-        batch = client.batches.retrieve(
-            batch_id=batch_id,
-            extra_headers=extra_headers,
-            extra_query=extra_query,
-            extra_body=extra_body,
-            timeout=timeout,
-        )
+    single = isinstance(batch_id, str)
+    ret_order = [batch_id] if single else list(batch_id)
+    batches = {}
 
-        if callback is not None:
-            callback(batch)
+    waiting_on = set(ret_order)
+    while waiting_on:
+        for batch_id in tuple(waiting_on):
+            batch = client.batches.retrieve(
+                batch_id=batch_id,
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+            )
 
-        if batch.status in FINISHED_STATES:
-            return batch
+            if callback is not None:
+                callback(batch)
 
-        time.sleep(interval)
+            if batch.status in FINISHED_STATES:
+                if finished_callback is not None:
+                    finished_callback(batch)
+
+                waiting_on.remove(batch_id)
+                batches[batch_id] = batch
+
+        if waiting_on:
+            time.sleep(interval)
+
+    ret = [batches[bid] for bid in ret_order]
+    return ret[0] if single else ret
